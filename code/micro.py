77228fae932035777511b9e4527e7b553d672df8
Emanuel Almeida <40006289+almemanuel@users.noreply.github.com>
2021-05-11 16:10:34 -0300

    On branch main 	modified:   code/micro.py

--- code/micro.py
+++ code/micro.py
@@ -3,8 +3,8 @@ from httpx import get
 
 
 def dollar():
-    response = get('https://economia.awesomeapi.com.br/json/daily/USD-BRL/1').json
-    return response
+    response = get('https://economia.awesomeapi.com.br/json/all/USD').json()
+    return response['USD']['high']
 
 
 fire.Fire()
